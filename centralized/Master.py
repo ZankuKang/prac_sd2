774f40d84cb7283c92239caf71644be70fcf09ff
Luis Miguel Soriano Frutos <luismiguel.soriano@estudiants.urv.cat>
2024-05-24 11:26:58 +0200

    Luis Miguel Soriano i Pere Daniel Roca: Centralized implementation (Master.py, Service)

---
+++ centralized/Master.py
@@ -0,0 +1,50 @@
+from proto import store_pb2, store_pb2, store_pb2_grpc
+import time
+import grpc
+from concurrent.futures import ThreadPoolExecutor
+from concurrent import futures
+from proto.basicproto import master_servicer
+
+# Basic features
+storage = {}
+registeredNodes = []
+
+
+def start_service():
+    # creates the grpc server on master node
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    store_pb2_grpc.add_KeyValueStoreServicer_to_server(master_servicer, server)
+    # Defines where the server will listen
+    server.add_insecure_port('localhost:32770')
+    server.start()
+    server.wait_for_termination()
+
+def Two_PC(put_request, context):
+    # We get the nodes that are registered in master
+    registeredNodes = master_servicer.getDiscoverQueue()
+    for node in  registeredNodes:
+        channel = grpc.insecure_channel(node) # node = IP:PORT
+        stub = store_pb2_grpc.KeyValueStoreStub(channel)
+        request = store_pb2.CommitRequest(key=put_request.key, value=put_request.value)
+        can_node_commit = stub.canCommit(request)
+        if not can_node_commit.success:
+            return False
+    for node in registeredNodes:
+        channel = grpc.insecure_channel(node)
+        stub = store_pb2_grpc.KeyValueStoreStub(channel)
+        commit = store_pb2.CommitRequest(key=put_request.key, value=put_request.value)
+        have_commited = stub.doCommit(commit)
+        if not  have_commited.success:
+            return False
+    return True
+
+
+def main():
+    master_servicer.set_storage(storage)
+    master_servicer.setDicoverQueue(registeredNodes)
+
+    start_service()
+
+    while True:
+        time.sleep(100000)
+
--- proto/basicproto.py
+++ proto/basicproto.py
@@ -45,12 +45,28 @@ class BasicAPIService:
     def registerSlave(self, slave_info, context):
         return store_pb2.Response(success=False)
 
+
 class MasterNodeService(BasicAPIService):
     def __init__(self):
         super.__init__()
+        self.dis_queue = []
 
     def put(self, put_request, context):
         # We have to implement the two phase commit before
         self.storage[put_request.key] = put_request.value
         return store_pb2.PutResponse(success=True)
-    
+
+    def setDicoverQueue(self, dis_queue):
+        self.dis_queue = dis_queue
+
+    def registerSlave(self, slave_info, context):
+        self.dis_queue.append(slave_info.address)  # IP:Port
+        storageReplication = json.dumps(self.storage)
+        return store_pb2.Response(store=storageReplication, success=True)
+
+    def getDiscoverQueue(self):
+        return self.dis_queue
+
+
+centralized_servicer = BasicAPIService()
+master_servicer = MasterNodeService()
